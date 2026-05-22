564a439a4b0214fcecb0ac6522b6c5208f7aaeb7
FabriAV <fabricio.acosta.vasquez@outlook.com>
2026-05-22 00:04:53 -0500

    feat: implement jwt, get (categories and products)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Elysia, t } from 'elysia';
+import { jwt } from '@elysia/jwt'
 import { cors } from '@elysia/cors'
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient, Prisma } from "../generated/prisma/client";
@@ -10,33 +11,64 @@ const prisma = new PrismaClient({ adapter });
 
 const app = new Elysia()
     .use(cors())
+    .use(
+        jwt({
+            name: 'jwt',
+            secret: process.env.JWT_SECRET!
+        })
+    )
     .get('/', async () => 'ok')
-    .get('/users', async () => { 
-        return await prisma.user.findMany() })
+    .get('/categories', async () => { return await prisma.category.findMany()})
+    .get('/users', async () => { return await prisma.user.findMany() })
+    .get('/products', async ({ query }) => {
+        return await prisma.product.findMany({
+            where:
+            {
+                name: query.name,
+                categories: {
+                    some: {
+                        name: query.categories
+                    }
+                },
+                price: {
+                    gte: query.minPrice ? parseFloat(query.minPrice) : undefined,
+                    lte: query.maxPrice ? parseFloat(query.maxPrice) : undefined
+                }
+            }
+        })
+    }, {
+        query: t.Object({
+            name: t.Optional(t.String()),
+            categories: t.Optional(t.String()),
+            minPrice: t.Optional(t.String()),
+            maxPrice: t.Optional(t.String()),
+        })
+    })
     .group('/auth', (app) => app
         .post('/sign-up', async ({ body }) => {
-            try{return await prisma.user.create({
-                data:
-                {
-                    name: body.name,
-                    email: body.email,
-                    password: encryption(body.password),
-                    address: body.address,
-                    phone: body.phone,
-                }
-            })}
-            catch(e){
+            try {
+                return await prisma.user.create({
+                    data:
+                    {
+                        name: body.name,
+                        email: body.email,
+                        password: encryption(body.password),
+                        address: body.address,
+                        phone: body.phone,
+                    }
+                })
+            }
+            catch (e) {
                 if (e instanceof Prisma.PrismaClientKnownRequestError) {
                     if (e.code === 'P2002') {
-                      // target will be ['email']
-                      if ((e.meta?.target as String[]).includes('email')) {
-                        return { message: 'Email already registered' }
+                        if ((e.meta?.target as String[]).includes('email')) {
+                            return { message: 'Email already registered' }
+                        }
+                        if ((e.meta?.target as String[]).includes('phone')) {
+                            return { message: 'Phone already registered' }
+                        }
                     }
-                    if ((e.meta?.target as String[]).includes('phone')) {
-                        return { message: 'Phone already registered' }
-                    }
-                    }
-                  }
+                }
             }
         },
             {
@@ -48,7 +80,7 @@ const app = new Elysia()
                     phone: t.String()
                 })
             })
-        .post('/sign-in', async ({ body }) => {
+        .post('/sign-in', async ({ body, jwt, cookie: { auth } }) => {
             const user = await prisma.user.findUnique({
                 where:
                 {
@@ -56,9 +88,20 @@ const app = new Elysia()
                 }
             })
 
-           return encryption(body.password) == user?.password;
+            if (encryption(body.password) == user?.password) {
+                const token = await jwt.sign({
+                    id: user.id,
+                    email: user.email,
+                    isAdmin: user.isAdmin
+                })
+                auth?.set({ value: token, httpOnly: true, maxAge: 7 * 86400 })
+                return token
+            }
 
-            
+            return { message: 'Invalid credentials' }
+            ;
+
+
         },
             {
                 body: t.Object({
@@ -67,8 +110,20 @@ const app = new Elysia()
                 })
             }
         )
-            
     )
+    .post('/categories', async ({body}) => {
+        return await prisma.category.create({
+            data:{
+                name: body.name,
+                description: body.description
+            }
+        })
+    },{
+        body: t.Object({
+            name: t.String(),
+            description: t.String()      
+        })
+    })
     .listen(3000)
 
 console.log(
